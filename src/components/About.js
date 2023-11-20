ae2ff070730b1389b03f763bada782e4e3fb6bcd
DavidButterton <sadhu.butterton@gmail.com>
2023-11-20 17:19:46 -0600

    assignment complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { image } from "../data/data";
 
+
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+     <h2>About Me</h2>
+     <p>I am a multi-faceted being of light that explores the universe from all angles</p>
+     <img src={image} alt="I made this"></img>
+  </div>
+)
 }
 
 export default About;
